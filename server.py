5569063cf6a425f28e2ae298d9d450e5f582dd77
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-04-12 23:35:50 +0200

    finished server-side

--- server.py
+++ server.py
@@ -1,6 +1,19 @@
+from flask import Flask, request, jsonify
 from deepface import DeepFace
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+app = Flask(__name__)
 
-face_analysis = DeepFace.analyze(img_path="woman.jpg", enforce_detection=False)
+@app.route('/api/upload', methods=['POST'])
+def analyze():
+    img = request.body['image']
+    result = DeepFace.analyze(img, enforce_detection=False)
+    dominant_emotion = result[0]['dominant_emotion']
+    return jsonify({'dominant_emotion': dominant_emotion})
 
-print(face_analysis[0]["dominant_emotion"])
+if __name__ == '__main__':
+    host = os.getenv('DOMAIN', 'localhost')
+    port = int(os.getenv('PORT', 3001))
+    app.run(host=host, port=port)
