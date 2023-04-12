ef6ea9f6291db484e34f307cc0c791d5cc4e2af2
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-04-12 23:01:15 +0200

    fixed extraction

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from deepface import DeepFace
 
-face_analysis = DeepFace.analyze(img_path="woman.jpg")
-print(face_analysis["emotion"])
-print(face_analysis["dominant_emotion"])
+
+face_analysis = DeepFace.analyze(img_path="woman.jpg", enforce_detection=False)
+
+print(face_analysis[0]["dominant_emotion"])
