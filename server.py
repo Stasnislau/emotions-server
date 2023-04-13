622fa2071ccc0d1b925cdf98816ecb524ea55ee2
Stanislau Ryzhkou <stanislaurv@gmail.com>
2023-04-13 12:10:16 +0200

    fixed server

--- server.py
+++ server.py
@@ -1,19 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, flash, redirect
 from deepface import DeepFace
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/upload', methods=['POST'])
 def analyze():
-    img = request.body['image']
-    result = DeepFace.analyze(img, enforce_detection=False)
-    dominant_emotion = result[0]['dominant_emotion']
-    return jsonify({'dominant_emotion': dominant_emotion})
+    file = request.files['image']
+    file.save('image.png')
+    [result] = DeepFace.analyze('image.png', enforce_detection=False)
+    dominant_emotion = result['dominant_emotion']
+    return jsonify({'mood': dominant_emotion})
 
 if __name__ == '__main__':
     host = os.getenv('DOMAIN', 'localhost')
     port = int(os.getenv('PORT', 3001))
+    print(app.url_map)
     app.run(host=host, port=port)
